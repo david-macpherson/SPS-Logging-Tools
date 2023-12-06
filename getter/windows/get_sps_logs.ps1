a09a28cc02b99220630d19b92dc3cf713b93cdbe
David MacPherson <david@tensorworks.com.au>
2023-12-06 15:02:20 +1000

    Added Error checking for namespace

--- getter/windows/get_sps_logs.ps1
+++ getter/windows/get_sps_logs.ps1
@@ -1,3 +1,46 @@
+# Set up script paramaters
+param(
+    [String]$namespace="",
+    [switch]$Help=$false,
+    [switch]$h=$false,
+    [switch]$StopJobs=$false
+)
+
+
+# If the -h or -Help flags are present show the help message and exit
+if ($Help -Or $h){
+    write-host "Usage: $0 [OPTIONS]"
+    write-host "Options:"
+    write-host "-h | -Help       Display this help message"
+    write-host "-namespace  Overides the kubectl context namespace"
+    exit 0
+}
+
+if ($StopJobs) {
+    Write-Host "Stopping all background logging jobs, This can take some time"
+    get-job | stop-job
+    Write-Host "All logging jobs have been stopped"
+    exit 0
+}
+
+# Get the current context
+$context = $(kubectl config current-context)
+
+# Check if the namespace flag is empty
+if ($namespace -eq "") {
+
+    # Set the namespace var to the contexts namespace
+    $namespace = $(kubectl config view --minify -o jsonpath='{..namespace}')
+
+    # Check if the namespace is still null
+    if ($namespace -eq $null) {
+             
+        # Set the namespace to default as a fallback
+        Write-Host "Namespace not set in the context: $($context) falling back to default"
+        $namespace = "default"
+    }
+}
+
 # Const for the log directory
 $LOG_OUTPUT_DIR="$($PWD)\logs"
 
@@ -13,6 +56,8 @@ New-Item -ItemType "directory" -Path $LOG_OUTPUT_DIR | Out-Null
 
 # To keep track of what jobs have been started
 $jobTable = @{}
+
+Write-Host "Getting pods logs for context: $($context) in namespace: $($namespace) press crtl+c or close the powershell window to stop the logging jobs"
 
 while ($True) {
     # Get a list of pods
@@ -37,7 +82,7 @@ while ($True) {
                 Write-Host $($POD.metadata.name) - $($CONTAINER.name)
                 
                 # Start the container log capture
-                $job = Start-Job -ScriptBlock { kubectl logs --follow $args[0] --container $args[1] > $args[2] } -ArgumentList $POD.metadata.name, $CONTAINER.name, $LOG_FILE | Out-Null
+                $job = Start-Job -ScriptBlock { kubectl logs --follow $args[0] --container $args[1] --namespace $args[2] > $args[3] } -ArgumentList $POD.metadata.name, $CONTAINER.name, $namespace, $LOG_FILE | Out-Null
                 
                 # Add the job id to the job table
                 $jobTable[$LOG_FILENAME] = $job.Id
